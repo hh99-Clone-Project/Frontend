a095cb66464480e26bcbc6d516fc6437f6b629e8
Kopite93 <1000nulee@naver.com>
2022-04-15 17:12:30 +0900

    btn

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import "./index.css";
 import App from "./shared/App";
 import { Provider } from "react-redux";
 import store from "./redux/configStore";
+import Button from "./elements/Button";
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-<<<<<<< HEAD
   document.getElementById("root")
-=======
-  document.getElementById('root')
->>>>>>> 1a2883e78dda8a5b4068edfd8c1a3322028cea04
 );
